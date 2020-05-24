75fc74e8976ec6c0d50483c44e6fbd15d239e171
yimialmonte <yelmialmonte@gmail.com>
2020-05-24 07:10:05 -0400

    set up routes and controllers with api versioning

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import Express from 'express'
 import Mongoose from 'mongoose'
 import config from '../config'
+import v1Router from '../routes'
 
 Mongoose.connect(config.databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const app = Express()
+app.use(v1Router)
 
 app.listen(3000, () => {
     console.log('Server started succesfully')
