42731e0c7914aa2dcfe823b27f0831daccf2dfa8
yimialmonte <yelmialmonte@gmail.com>
2020-05-26 11:29:30 -0400

    configure css loader to the project

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Main from './pages/Main.vue'
+import './styles/main.css'
 
 const app = new Vue({
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const path = require('path')
 
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
+const MiniExtraCssPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
 
@@ -26,10 +28,20 @@ module.exports = {
         use: {
           loader: 'vue-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniExtraCssPlugin.loader,
+          'css-loader'
+        ]
       }
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new MiniExtraCssPlugin({
+      filename: 'app.css'
+    })
   ]
 }
