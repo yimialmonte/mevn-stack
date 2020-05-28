d1736201da87f185ffda3270fac1723170718d01
yimialmonte <yelmialmonte@gmail.com>
2020-05-28 16:43:59 -0400

    installed and setup vue router

--- client/index.js
+++ client/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Main from './pages/Main.vue'
 import './styles/main.css'
+import router from './routes'
+import Router from 'vue-router'
+
+Vue.use(Router)
 
 const app = new Vue({
 
   el: '#app',
+  router,
   render: h => h(Main)
 
 });
