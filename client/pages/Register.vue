0da0cc185a53754cf514378c6ec1211409d664d7
yimialmonte <yelmialmonte@gmail.com>
2020-05-30 08:29:08 -0400

    design register page

--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Sign ups</h1>
+  <div class="container w-full mx-auto my-16">
+    <div class="max-w-xs mx-auto">
+      <h2 class="text-center text-gold">Register</h2>
+      <div class="w-full bg-white shadow mt-5 rounded-sm p-8">
+        <input
+          type="text"
+          class="w-full bg-brown-lightest mb-5 p-3 focus:outline-none text-brown px-2 text-sm"
+          placeholder="Enter your name"
+        />
+        <input
+          type="text"
+          class="w-full bg-brown-lightest p-3 mb-5 focus:outline-none text-brown px-2 text-sm"
+          placeholder="Enter your email"
+        />
+        <input
+          type="text"
+          class="w-full bg-brown-lightest p-3 focus:outline-none text-brown px-2 text-sm"
+          placeholder="Enter your password"
+        />
+        <button
+          class="w-full mt-3 text-lg py-3 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-lighter"
+        >
+          Sign Up
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
