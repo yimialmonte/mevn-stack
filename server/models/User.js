1cbaf2f459f38fc93d73588ad26eb82f828cdf1b
yimialmonte <yelmialmonte@gmail.com>
2020-06-02 11:49:07 -0400

    hashing user password in db

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import Bycrypt from 'bcryptjs'
+import RandomString from 'randomstring'
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -14,6 +16,14 @@ const UserSchema = new mongoose.Schema({
   emailConfirmAt: Date,
 
   emailConfirmCode: String
+})
+
+UserSchema.pre('save', function() {
+  this.password = Bycrypt.hashSync(this.password)
+
+  this.emailConfirmCode = RandomString.generate(72)
+
+  this.createdAt = new Date()
 })
 
 export default new mongoose.model('User', UserSchema)
